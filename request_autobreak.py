347ce0c2c661b7d7cde5cf1187c7b66fd4124e79
SCendlak <95042172+SCendlak@users.noreply.github.com>
2021-12-09 03:22:07 +0100

    working inewi by requests

--- request_autobreak.py
+++ request_autobreak.py
@@ -14,23 +14,39 @@ class Autobreak:
         payload = {"Email": inewi_name,"Password": inewi_passwd}
         ua = UserAgent()
         with requests.Session() as session:
-            landing_page = session.get(self.inewi_url, verify=False, headers={"User-Agent":ua.google})
+            session.auth = (inewi_name,inewi_passwd)
+            headers = {"User-Agent": ua['google chrome']}
+            landing_page = session.get(self.inewi_url, verify=False, headers=headers)
             soup = BeautifulSoup(session.get(self.inewi_url).content, 'html.parser')
             try:
                 token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
             except Exception as e:
                 print("Got unhandled exception %s" % str(e))
             payload.update({"__RequestVerificationToken": str(token)})
-            session.post(landing_page.url, data=payload)
+            session.post(landing_page.url, data=payload, headers=headers)
             payload.clear()
             payload.update({
-                "UserLogin": inewi_name,
+                "UserEmail": inewi_name,
                 "Password": inewi_passwd,
                 "TimeZone": "Europe/Warsaw",
                 "TimeUtcUnix": str(int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()))
             })
-            second_page = session.post("https://inewi.pl/kiosk/", data=payload)
-            print(session.cookies,landing_page.text, second_page.text)
+            second_page = session.post("https://inewi.pl/kiosk/OnlineRcp/LoginByEmailOrPhone", data=payload, headers=headers)
+            statuses = second_page.json()['statuses']
+            for d in statuses:
+                if d['Name'] == 'Przerwa':
+                    payload.clear()
+                    payload.update({
+                        "StatusId": d['Id'],
+                        "QrCode": second_page.json()['qrCode'],
+                        "TimeUtcUnix": second_page.json()['timeUtcUnix'],
+                        "TimeLocalUnix": second_page.json()['timeLocalUnix'],
+                        "IsEnd": d['IsEnd'],
+                        "TimeZoneId": second_page.json()['timeZoneId']
+                    })
+            session.post('https://inewi.pl/kiosk/OnlineRcp/SetStatus', data=payload, headers=headers)
+
+            #TODO: change hardcoded links, add functions to post entry and exit in inewi
 
     def log_livefruit(self, livefruit_name, livefruit_passwd):
         print(livefruit_name, livefruit_passwd)
